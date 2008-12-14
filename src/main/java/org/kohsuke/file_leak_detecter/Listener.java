1e40e451ee132ef77f7e877c732df6c11f8b0d16
kohsuke <null>
2008-12-14 06:50:26 +0000

    improving what we record

--- src/main/java/org/kohsuke/file_leak_detecter/Listener.java
+++ src/main/java/org/kohsuke/file_leak_detecter/Listener.java
@@ -18,14 +18,27 @@ public class Listener {
     private static final class Record {
         public final File file;
         public final Exception stackTrace = new Exception();
+        public final String threadName;
 
         private Record(File file) {
             this.file = file;
+            // keeping a Thread would potentially leak a thread, so let's just do a name
+            this.threadName = Thread.currentThread().getName();
         }
 
         public void dump(PrintStream ps) {
-            ps.println(file);
-            stackTrace.printStackTrace(ps);
+            ps.println(file+" by "+threadName);
+            StackTraceElement[] trace = stackTrace.getStackTrace();
+            int i=0;
+            // skip until we find the Method.invoke() that called us
+            for (; i<trace.length; i++)
+                if(trace[i].getClassName().equals("java.lang.reflect.Method")) {
+                    i++;
+                    break;
+                }
+            // print the rest
+            for (; i < trace.length; i++)
+                ps.println("\tat " + trace[i]);
         }
     }
 
@@ -35,14 +48,23 @@ public class Listener {
     private static final Map<Object,Record> TABLE = new HashMap<Object,Record>();
 
     /**
+     * Called when a new file is opened.
      *
      * @param _this
      *      {@link FileInputStream}, {@link FileOutputStream}, or {@link RandomAccessFile}.
+     * @param f
+     *      File being opened.
      */
     public static synchronized void open(Object _this, File f) {
         TABLE.put(_this,new Record(f));
     }
 
+    /**
+     * Called when a file is closed.
+     *
+     * @param _this
+     *      {@link FileInputStream}, {@link FileOutputStream}, or {@link RandomAccessFile}.
+     */
     public static synchronized void close(Object _this) {
         TABLE.remove(_this);
     }
