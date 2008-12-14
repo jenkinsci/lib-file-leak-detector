a9ffb763ca486d0d5cef19b10483724207072027
kohsuke <null>
2008-12-14 06:42:29 +0000

    use a table to track files that are currently open.

--- src/main/java/org/kohsuke/file_leak_detecter/Listener.java
+++ src/main/java/org/kohsuke/file_leak_detecter/Listener.java
@@ -3,6 +3,9 @@ package org.kohsuke.file_leak_detecter;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileInputStream;
+import java.io.RandomAccessFile;
+import java.util.Map;
+import java.util.HashMap;
 
 /**
  * Intercepted JDK calls land here.
@@ -10,19 +13,33 @@ import java.io.FileInputStream;
  * @author Kohsuke Kawaguchi
  */
 public class Listener {
-    public static void open(FileOutputStream fos, File f) {
-        System.out.println(fos+" opened "+f);
+
+    private static final class Record {
+        public final File file;
+        public final Exception stackTrace = new Exception();
+
+        private Record(File file) {
+            this.file = file;
+        }
     }
 
-    public static void close(FileOutputStream fos) {
-        System.out.println(fos+" closed");
+    /**
+     * Files that are currently open.
+     */
+    private static final Map<Object,Record> TABLE = new HashMap<Object,Record>();
+
+    /**
+     *
+     * @param _this
+     *      {@link FileInputStream}, {@link FileOutputStream}, or {@link RandomAccessFile}.
+     */
+    public static void open(Object _this, File f) {
+        System.out.println(_this+" opened "+f);
+        TABLE.put(_this,new Record(f));
     }
 
-    public static void open(FileInputStream fos, File f) {
-        System.out.println(fos+" opened "+f);
-    }
-
-    public static void close(FileInputStream fos) {
-        System.out.println(fos+" closed");
+    public static void close(Object _this) {
+        System.out.println(_this+" closed");
+        TABLE.remove(_this);
     }
 }
--- src/main/java/org/kohsuke/file_leak_detecter/Main.java
+++ src/main/java/org/kohsuke/file_leak_detecter/Main.java
@@ -19,15 +19,15 @@ import java.lang.instrument.UnmodifiableClassException;
 public class Main {
     public static void premain(String agentArguments, Instrumentation instrumentation) throws UnmodifiableClassException, IOException {
         System.out.println("Installed");
-        TransformerImpl t = new TransformerImpl(
+        instrumentation.addTransformer(new TransformerImpl(
             newSpec(FileOutputStream.class,"(Ljava/io/File;Z)V"),
             newSpec(FileInputStream.class, "(Ljava/io/File;)V"),
             newSpec(RandomAccessFile.class,"(Ljava/io/File;Ljava/lang/String;)V")
-        );
-        instrumentation.addTransformer(t,true);
+        ),true);
         instrumentation.retransformClasses(
                 FileInputStream.class,
-                FileOutputStream.class);
+                FileOutputStream.class,
+                RandomAccessFile.class);
 
         // test code
         FileOutputStream o = new FileOutputStream("target/dummy");
@@ -35,19 +35,23 @@ public class Main {
         o.close();
     }
 
+    /**
+     * Creates {@link ClassTransformSpec} that intercepts
+     * a constructor and the close method.
+     */
     private static ClassTransformSpec newSpec(final Class c, String constructorDesc) {
         return new ClassTransformSpec(c.getName().replace('.','/'),
             new MethodAppender("<init>", constructorDesc) {
                 protected void append(CodeGenerator g) {
                     g.invokeAppStatic("org.kohsuke.file_leak_detecter.Listener","open",
-                            new Class[]{c, File.class},
+                            new Class[]{Object.class, File.class},
                             new int[]{0,1});
                 }
             },
             new MethodAppender("close","()V") {
                 protected void append(CodeGenerator g) {
                     g.invokeAppStatic("org.kohsuke.file_leak_detecter.Listener","close",
-                            new Class[]{c},
+                            new Class[]{Object.class},
                             new int[]{0});
                 }
             }
