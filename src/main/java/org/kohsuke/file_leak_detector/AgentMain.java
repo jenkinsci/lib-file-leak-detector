centic9/file-leak-detector

Add possibility to exclude open files by their stacktrace

src/main/java/org/kohsuke/file_leak_detector/AgentMain.java
@@ -9,7 +9,9 @@ import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketImpl;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
@@ -62,6 +64,28 @@ public class AgentMain {
                 } else
                 if(t.startsWith("error=")) {
                     Listener.ERROR = new PrintWriter(new FileOutputStream(t.substring(6)));
+                } else
+                if(t.startsWith("excludes=")) {
+                	List<String> lines = new ArrayList<String>();
+                    BufferedReader reader = new BufferedReader(new FileReader(t.substring(9)));
+                    try {
+	                    String line = reader.readLine();
+	                    while (line != null) {
+	                        lines.add(line);
+	                        line = reader.readLine();
+	                    }
+                    } finally {
+                    	reader.close();
+                    }
+                    
+                    // add the entries from the excludes-file, but filter out empty ones
+                    Iterator<String> it = lines.iterator();
+                    while(it.hasNext()) {
+                    	String str = it.next().trim();
+                    	if(!str.isEmpty() && !str.startsWith("#")) {
+                    		Listener.EXCLUDES.add(str);
+                    	}
+                    }
                 } else {
                     System.err.println("Unknown option: "+t);
                     usageAndQuit();

jenkinsci/lib-file-leak-detector

src/main/java/org/kohsuke/file_leak_detector/AgentMain.java
@@ -13,6 +13,7 @@ import org.kohsuke.file_leak_detector.transform.TransformerImpl;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -81,6 +82,25 @@ public class AgentMain {
                             Listener.dump(System.err);
                         }
                     });
+                } else
+                if(t.startsWith("excludes=")) {
+                    BufferedReader reader = new BufferedReader(new FileReader(t.substring(9)));
+                    try {
+	                    while (true) {
+	                    	String line = reader.readLine();
+	                    	if(line == null) {
+	                    		break;
+	                    	}
+
+	                    	String str = line.trim();
+	                        // add the entries from the excludes-file, but filter out empty ones and comments
+	                    	if(!str.isEmpty() && !str.startsWith("#")) {
+	                    		Listener.EXCLUDES.add(str);
+	                    	}
+	                    }
+                    } finally {
+                    	reader.close();
+                    }
                 } else {
                     System.err.println("Unknown option: "+t);
                     usageAndQuit();
@@ -164,6 +184,8 @@ public class AgentMain {
         System.err.println("  strong        - Don't let GC auto-close leaking file descriptors");
         System.err.println("  listener=S    - Specify the fully qualified name of ActivityListener class to activate from beginning");
         System.err.println("  dumpatshutdown- Don't let GC auto-close leaking file descriptors");
+        System.err.println("  excludes=File - Exclude any opened file where a line in the given exclude-file matches");
+        System.err.println("                  one of the lines from the stacktrace of the open-call.");
     }
 
     static List<ClassTransformSpec> createSpec() {