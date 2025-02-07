1e2e478cd4db904ef2628c6aa12a76ef16c4a283
Dominik Stadler <dominik.stadler@dynatrace.com>
2025-02-07 20:05:33 +0100

    Ignore SocketTest on Java 17+
    They do not work there any more due to changed
    Socket implementation in JDK 17+

--- src/test/java/org/kohsuke/file_leak_detector/instrumented/SocketTest.java
+++ src/test/java/org/kohsuke/file_leak_detector/instrumented/SocketTest.java
@@ -22,9 +22,11 @@ import java.util.List;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.ExceptionUtils;
 import org.kohsuke.file_leak_detector.Listener;
 
 /**
@@ -34,25 +36,36 @@ import org.kohsuke.file_leak_detector.Listener;
  * @author Kohsuke Kawaguchi
  */
 public class SocketTest {
-	static {
-		// disable keepAlive for sun.net.www.http.HttpClient
-		// as otherwise some connections are kept open for re-use
-		// https://docs.oracle.com/javase/7/docs/technotes/guides/net/http-keepalive.html
-		System.setProperty("http.keepAlive", "false");
-	}
+    static {
+        // disable keepAlive for sun.net.www.http.HttpClient
+        // as otherwise some connections are kept open for re-use
+        // https://docs.oracle.com/javase/7/docs/technotes/guides/net/http-keepalive.html
+        System.setProperty("http.keepAlive", "false");
+    }
 
-	@AfterEach
-	public void tearDown() {
+    @BeforeAll
+    public static void beforeAll() {
+        try {
+            SocketImpl.class.getDeclaredField("socket");
+        } catch (NoSuchFieldException e) {
+            // Java 17+ changed the implementation of Sockets and
+            // so the current approach does not work there anymore
+            // for now we gracefully handle this and do keep file-leak-detector
+            // useful for other types of file-handle-leaks
+            Assumptions.abort("Cannot run SocketTest with Java 17 or newer, had: " + ExceptionUtils.readStackTrace(e));
+        }
+    }
+
+    @AfterEach
+    public void tearDown() {
         List<Listener.Record> files = new ArrayList<>(Listener.getCurrentOpenFiles());
 
         // exclude some false-positives when running tests via Maven
         files.removeIf(record ->
-            record.toString().contains("/dev/random") ||
-            record.toString().contains("/dev/urandom")
-        );
+                record.toString().contains("/dev/random") || record.toString().contains("/dev/urandom"));
 
         assertEquals(0, files.size(), "Should not have any open files now, but had: " + files);
-	}
+    }
 
     @Test
     public void testSocketChannelLeakDetection() throws IOException, InterruptedException {
@@ -70,11 +83,12 @@ public class SocketTest {
                 throw new UncheckedIOException(ioe);
             }
         });
-        SocketChannel socketChannel = SocketChannel.open(new InetSocketAddress("", serverSocket.socket().getLocalPort()));
+        SocketChannel socketChannel = SocketChannel.open(
+                new InetSocketAddress("", serverSocket.socket().getLocalPort()));
 
         while (sockets.size() < 1) {
-			//noinspection BusyWait
-			Thread.sleep(500);
+            //noinspection BusyWait
+            Thread.sleep(500);
         }
 
         assertEquals(1, sockets.size());
@@ -105,7 +119,7 @@ public class SocketTest {
 
     @Test
     public void testSocketLeakDetection() throws IOException, InterruptedException {
-		assertEquals(0, getSockets());
+        assertEquals(0, getSockets());
 
         final ExecutorService es = Executors.newCachedThreadPool();
 
@@ -124,8 +138,8 @@ public class SocketTest {
         Socket s = new Socket("localhost", ss.getLocalPort());
 
         while (sockets.size() < 1) {
-			//noinspection BusyWait
-			Thread.sleep(500);
+            //noinspection BusyWait
+            Thread.sleep(500);
         }
 
         assertEquals(1, sockets.size());
@@ -164,60 +178,60 @@ public class SocketTest {
         return sockets;
     }
 
-	@Test
-	public void testHttpUrlConnection() throws IOException, ClassNotFoundException {
-		assertEquals(0, getSockets(), "No socket should be open before connecting");
+    @Test
+    public void testHttpUrlConnection() throws IOException, ClassNotFoundException {
+        assertEquals(0, getSockets(), "No socket should be open before connecting");
 
-		HttpURLConnection conn = (HttpURLConnection) new URL("https://www.google.com").openConnection();
-		try {
-			conn.setConnectTimeout(10000);
-			conn.setReadTimeout(10000);
-			conn.setDoOutput(false);
-			conn.setDoInput(true);
-			conn.connect();
-			assertEquals(HttpURLConnection.HTTP_OK, conn.getResponseCode());
+        HttpURLConnection conn = (HttpURLConnection) new URL("https://www.google.com").openConnection();
+        try {
+            conn.setConnectTimeout(10000);
+            conn.setReadTimeout(10000);
+            conn.setDoOutput(false);
+            conn.setDoInput(true);
+            conn.connect();
+            assertEquals(HttpURLConnection.HTTP_OK, conn.getResponseCode());
 
-			assertEquals(1, getSockets(), "We should have a socket open now");
-		} finally {
-			conn.disconnect();
-		}
+            assertEquals(1, getSockets(), "We should have a socket open now");
+        } finally {
+            conn.disconnect();
+        }
 
-		assertEquals(0, getSockets(), "No socket should be open after closing the connection");
-	}
+        assertEquals(0, getSockets(), "No socket should be open after closing the connection");
+    }
 
-	@Test
-	public void testHttpUrlConnectionWithRead() throws IOException, ClassNotFoundException {
-		assertEquals(0, getSockets(), "No socket should be open before connecting");
+    @Test
+    public void testHttpUrlConnectionWithRead() throws IOException, ClassNotFoundException {
+        assertEquals(0, getSockets(), "No socket should be open before connecting");
 
-		HttpURLConnection conn = (HttpURLConnection) new URL("https://www.google.com").openConnection();
-		try {
-			conn.setConnectTimeout(10000);
-			conn.setReadTimeout(10000);
-			conn.setDoOutput(false);
-			conn.setDoInput(true);
-			conn.connect();
-			assertEquals(HttpURLConnection.HTTP_OK, conn.getResponseCode());
+        HttpURLConnection conn = (HttpURLConnection) new URL("https://www.google.com").openConnection();
+        try {
+            conn.setConnectTimeout(10000);
+            conn.setReadTimeout(10000);
+            conn.setDoOutput(false);
+            conn.setDoInput(true);
+            conn.connect();
+            assertEquals(HttpURLConnection.HTTP_OK, conn.getResponseCode());
 
+            assertEquals(1, getSockets(), "We should have a socket open now");
 
-			assertEquals(1, getSockets(), "We should have a socket open now");
+            // actually read the contents, even if we are not using it to simulate a full download of the data
+            try (InputStream stream = conn.getInputStream();
+                    ByteArrayOutputStream memStream = new ByteArrayOutputStream(
+                            conn.getContentLength() == -1 ? 40000 : conn.getContentLength())) {
+                byte[] b = new byte[4096];
+                int len;
+                while ((len = stream.read(b)) > 0) {
+                    memStream.write(b, 0, len);
+                }
 
-			// actually read the contents, even if we are not using it to simulate a full download of the data
-			try (InputStream stream = conn.getInputStream();
-				ByteArrayOutputStream memStream = new ByteArrayOutputStream(conn.getContentLength() == -1 ? 40000 : conn.getContentLength())) {
-				byte[] b = new byte[4096];
-				int len;
-				while ((len = stream.read(b)) > 0) {
-					memStream.write(b, 0, len);
-				}
+                memStream.flush();
+            }
 
-				memStream.flush();
-			}
+            assertEquals(0, getSockets(), "Socket should be closed when stream is exhausted");
+        } finally {
+            conn.disconnect();
+        }
 
-			assertEquals(0, getSockets(), "Socket should be closed when stream is exhausted");
-		} finally {
-			conn.disconnect();
-		}
-
-		assertEquals(0, getSockets(), "No socket should be open after closing the connection");
-	}
+        assertEquals(0, getSockets(), "No socket should be open after closing the connection");
+    }
 }
